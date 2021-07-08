45b9ea0eaf400ac4641eab21a7216e4e83f7630b
jazboat <84553765+jazboat@users.noreply.github.com>
2021-07-08 22:29:37 +1000

    Update game.js

--- game.js
+++ game.js
@@ -1,19 +1,9 @@
-const topLeftSqu = document.querySelector('.top-left');
-const topSqu = document.querySelector('.top');
-const topRightSqu = document.querySelector('.top-right');
-const middleLeftSqu = document.querySelector('.middle-left');
-const middleSqu = document.querySelector('.middle');
-const middleRightSqu = document.querySelector('.top-left');
-const bottomLeftSqu = document.querySelector('.bottom-left');
-const bottomSqu = document.querySelector('.bottom');
-const bottomRightSqu = document.querySelector('.bottom-left');
-const gameDisplay = document.querySelector('.game-status');
-const restartBtn = document.querySelector('.restart-btn');
-var playerXStatus = document.querySelector('.X');
-var playerOStatus = document.querySelector('.O');
+var restartBtn = document.querySelector('.restart-btn');
+var playerX = document.querySelector('.player-x');
+var playerO = document.querySelector('.player-o');
 
 
-const allBoxes = document.querySelectorAll('.board-game div');
+var allBoxes = document.querySelectorAll('.box');
 
 var currentPlayer = 'X'
 var numberMoves = 0
@@ -29,30 +19,33 @@ function handleClick(event) {
 
     numberMoves ++ 
 
-    var s0 = topLeftSqu.textContent;
-    var s1 = topSqu.textContent;
-    var s2 = topRightSqu.textContent;
-    var s3 = middleLeftSqu.textContent;
-    var s4 = middleSqu.textContent;
-    var s5 = middleRightSqu.textContent;
-    var s6 = bottomLeftSqu.textContent;
-    var s7 = bottomSqu.textContent;
-    var s8 = bottomRightSqu.textContent;
-    
-    var i = playerXStatus
+    var s0 = document.querySelector('.top-left').textContent;
+    var s1 = document.querySelector('.top').textContent;
+    var s2 = document.querySelector('.top-right').textContent;
+    var s3 = document.querySelector('.middle-left').textContent;
+    var s4 = document.querySelector('.middle').textContent;
+    var s5 = document.querySelector('.middle-right').textContent;
+    var s6 = document.querySelector('.bottom-left').textContent;
+    var s7 = document.querySelector('.bottom').textContent;
+    var s8 = document.querySelector('.bottom-right').textContent;
+
 
     if (s0 === 'X' && s1 === 'X' && s2 === 'X') {
         allBoxes[0].classList.add('blink');
         allBoxes[1].classList.add('blink');
         allBoxes[2].classList.add('blink');
+    } else if (s3 === 'X' && s4 === 'X' && s5 === 'X') {
+        allBoxes[3].classList.add('blink');
+        allBoxes[4].classList.add('blink');
+        allBoxes[5].classList.add('blink');
+    } else if (s6 === 'X' && s7 === 'X' && s8 === 'X') {
+        allBoxes[6].classList.add('blink');
+        allBoxes[7].classList.add('blink');
+        allBoxes[8].classList.add('blink');
     } else if (s0 === 'X' && s3 === 'X' && s6 === 'X') {
         allBoxes[0].classList.add('blink');
         allBoxes[3].classList.add('blink');
         allBoxes[6].classList.add('blink');
-    } else if (s0 === 'X' && s4 === 'X' && s8 === 'X') {
-        allBoxes[0].classList.add('blink');
-        allBoxes[4].classList.add('blink');
-        allBoxes[8].classList.add('blink');
     } else if (s1 === 'X' && s4 === 'X' && s7 === 'X') {
         allBoxes[1].classList.add('blink');
         allBoxes[4].classList.add('blink');
@@ -65,27 +58,30 @@ function handleClick(event) {
         allBoxes[2].classList.add('blink');
         allBoxes[4].classList.add('blink');
         allBoxes[6].classList.add('blink');
-    } else if (s3 === 'X' && s4 === 'X' && s5 === 'X') {
-        allBoxes[3].classList.add('blink');
+    } else if (s0 === 'X' && s4 === 'X' && s8 === 'X') {
+        allBoxes[0].classList.add('blink');
         allBoxes[4].classList.add('blink');
-        allBoxes[5].classList.add('blink');
-    } else if (s6 === 'X' && s7 === 'X' && s8 === 'X') {
-        allBoxes[6].classList.add('blink');
-        allBoxes[7].classList.add('blink');
         allBoxes[8].classList.add('blink');
+///////////////////////////////////////////////////////////////////
+// START THE 0
+
     } else 
     if (s0 === 'O' && s1 === 'O' && s2 === 'O'){
         allBoxes[0].classList.add('blink');
         allBoxes[1].classList.add('blink');
         allBoxes[2].classList.add('blink');
+    } else if (s3 === 'O' && s4 === 'O' && s5 === 'O') {
+        allBoxes[3].classList.add('blink');
+        allBoxes[4].classList.add('blink');
+        allBoxes[5].classList.add('blink');
+    } else if (s6 === 'O' && s7 === 'O' && s8 === 'O') {
+        allBoxes[6].classList.add('blink');
+        allBoxes[7].classList.add('blink');
+        allBoxes[8].classList.add('blink');
     } else if (s0 === 'O' && s3 === 'O' && s6 === 'O') {
         allBoxes[0].classList.add('blink');
         allBoxes[3].classList.add('blink');
         allBoxes[6].classList.add('blink');
-    } else if (s0 === 'O' && s4 === 'O' && s8 === 'O') {
-        allBoxes[0].classList.add('blink');
-        allBoxes[4].classList.add('blink');
-        allBoxes[8].classList.add('blink');
     } else if (s1 === 'O' && s4 === 'O' && s7 === 'O') {
         allBoxes[1].classList.add('blink');
         allBoxes[4].classList.add('blink');
@@ -98,11 +94,7 @@ function handleClick(event) {
         allBoxes[2].classList.add('blink');
         allBoxes[4].classList.add('blink');
         allBoxes[6].classList.add('blink');
-    } else if (s3 === 'O' && s4 === 'O' && s5 === 'O') {
-        allBoxes[3].classList.add('blink');
-        allBoxes[4].classList.add('blink');
-        allBoxes[5].classList.add('blink');
-    } else if (s8 === 'O' && s7 === 'O' && s6 === 'O') {
+    } else if (s0 === 'O' && s4 === 'O' && s8 === 'O') {
         allBoxes[8].classList.add('blink');
         allBoxes[7].classList.add('blink');
         allBoxes[6].classList.add('blink');
@@ -118,6 +110,7 @@ function handleClick(event) {
 
     if (currentPlayer === 'X') {
         currentPlayer = 'O'
+
     } else {
         currentPlayer = 'X' 
     }
@@ -133,6 +126,7 @@ function restartHanlde() {
 
 
 restartBtn.addEventListener('click', restartHanlde);
+
 for (let i =0; i < allBoxes.length; i++) {
     allBoxes[i].addEventListener('click', handleClick)
 }
